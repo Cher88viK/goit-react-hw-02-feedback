05899c0a537616d33ddc2c8e864d4392b43c0191
Victoria <116460276+Cher88viK@users.noreply.github.com>
2023-08-23 22:46:11 +0300

    Add styles

---
+++ src/components/FeedbackOptions/Feedback.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-decoration: column;
+`;
+
+export const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #BF4F74;
+  color: #BF4F74;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  }
+`;
